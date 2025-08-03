5865847ae2dbc1c38357f8d29e8c6b82401b2fc3
Mohammad Salman Ansari <mohd.salman01a@gmail.com>
2025-08-03 19:59:44 +0530

    final

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,11 +1,13 @@
 import zod from "zod";
 
 const envSchema = zod.object({
-  DATABASE_URL: zod.string().nonempty(),
-  GOOGLE_CLIENT_ID: zod.string().nonempty(),
-  GOOGLE_CLIENT_SECRET: zod.string().nonempty(),
-  NEXTAUTH_URL: zod.string().nonempty(),
-  NEXTAUTH_SECRET: zod.string().nonempty(),
+  DATABASE_URL: zod.string().min(1, "DATABASE_URL is required"),
+  GOOGLE_CLIENT_ID: zod.string().min(1, "GOOGLE_CLIENT_ID is reqd"),
+  GOOGLE_CLIENT_SECRET: zod.string().min(1, "GOOGLE_CLIENT_SECRET is reqd"),
+  NEXTAUTH_URL: zod.string().min(1, "NEXTAUTH_URL is reqd"),
+  NEXTAUTH_SECRET: zod.string().min(1, "NEXTAUTH_SECRET is reqd"),
 });
 
 export const env = envSchema.parse(process.env);
+
+//GITHUB_ID: z.string().min(1, "GITHUB_ID is required"),
